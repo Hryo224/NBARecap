d45493f8f4329b4b46c01da4639c9efa5a6f1855
Hryo224 <hampton.young@uky.edu>
2017-03-15 20:44:05 -0400

    Modifying player

--- player.py
+++ player.py
@@ -4,11 +4,9 @@ players = nba.nba_data("players", 2016).get("league").get("standard")
 class Player:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
-            if value != "":
-                setattr(self, key, value)
-            else:
+            if value == "":
                 value = 0
-                setattr(self, key, value)
+            setattr(self, key, value)
         self.player = self.get_player_name(self.personId)
         
     def get_player_name(self, player_id):
